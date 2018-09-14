2c18dd66c3d926925ac60e8000834db31308494c
zhangjixu <1062904651@qq.com>
2018-09-14 09:59:44 +0800

    init project

---
+++ src/main/java/com/cn/zjx/controller/TestController.java
@@ -0,0 +1,43 @@
+package com.cn.zjx.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author: zhangjixu
+ * @CreateDate: 2018/9/14
+ * @Description:
+ * @Version: 1.0.0
+ */
+@Controller
+public class TestController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
+
+    @RequestMapping(value = "/")
+    public String index() {
+        LOGGER.error("modelAndView success");
+        return "/index.jsp";
+    }
+
+    @RequestMapping(value = "/page")
+    public String page() {
+        LOGGER.error("get method success");
+        return "/view/login.jsp";
+    }
+
+    @RequestMapping(value = "/data", produces = "application/json; charset=utf-8", method = RequestMethod.GET)
+    @ResponseBody
+    public String data() {
+        LOGGER.error("get data success");
+        return "success";
+    }
+
+
+
+}
---
+++ src/main/webapp/view/login.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jixuzhang
+  Date: 2018/9/14
+  Time: 上午9:08
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>Success</h1>
+</body>
+</html>
